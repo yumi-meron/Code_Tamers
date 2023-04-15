970ac00247f7a687fc25810413e785e63773d129
endragnee1 <anime.game7415@protonmail.com>
2023-04-15 20:36:05 +0300

    Part 1 of Project 1

---
+++ Project/Interset.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*This is Some reference to variables:
+    Inter is Interest
+    Inter_Rate is the rate of interest
+    Over_Inter is the excess from the interest
+    */
+    float Inter_Rate,Inter,Loan,Over_Inter=0,Payment,Anual_Per,Original_Loan;
+    string Array_Inter,Array_Anual,Array_Loan,Array_Full,Array_Over;
+    cout<<"Enter the Interst rate: \n->";
+    cin>>Inter_Rate;
+    cout<<"Enter the amount of loan: \n->";
+    cin>>Loan;
+    Original_Loan=Loan;
+    Payment=Original_Loan/20;
+    cout<<"Payment"<<"\t"<<"Loan"<<"\t\t"<<"Interest"<<"\t"<<"Annual(in %)";
+    do
+    {
+
+    }
+    while(Loan>=0)

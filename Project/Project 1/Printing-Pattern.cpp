03cb40eb83e10b4174f450ad52c785de23460009
endragnee1 <anime.game7415@protonmail.com>
2023-04-16 12:49:06 +0300

    Part 2 of Project 1

---
+++ Project/Project 1/Printing-Pattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n=10;
+//Setting triangle range to 0-9
+    for(int i=0; i<10;i++) 
+    {
+//Code fo the outer spacing
+        for(int k=n-i;k<10;++k) 
+        {
+            cout << " ";
+        }
+}
